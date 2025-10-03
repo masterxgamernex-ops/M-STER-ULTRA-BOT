175cfe03fb84fde109b317efd8d18243ec299b20
masterxgamernex-ops <masterxgamernex@gmail.com>
2025-10-02 23:06:39 -0400

    Update config.js

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ global.prefix = config.prefix || ".";
 
 // Lista de Owners
 global.owner = [
-    ["15167096032", "Owner", true],
+    ["584125877491", "Owner", true],
     ["115724051816605"],
     ["5491151545427"],
     ["595986172767"],
