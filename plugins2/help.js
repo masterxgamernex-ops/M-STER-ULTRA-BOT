d97f2f145fc85a4043b428a67dbf74d083a6b79e
masterxgamernex-ops <masterxgamernex@gmail.com>
2025-10-03 10:08:18 -0400

    Update help.js

--- plugins2/help.js
+++ plugins2/help.js
@@ -97,7 +97,7 @@ const handler = async (msg, { conn }) => {
     } else {
       // MENÚ POR DEFECTO NORMALITO
       caption = `
-╔⌬ ${M-STER ULTRA BOT }⌬╗
+╔⌬ ${ nombre }⌬╗
 ║   Menú por categorías  
 ╚═──────────────────═╝
 — 🔹 usa: 
